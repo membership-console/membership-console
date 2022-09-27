6a47aeb7b5fa0c1d178194a0b9a0ff75caf4dadc
averak <abe12@mccc.jp>
2022-09-28 05:37:28 +0900

    [feat] mat-cardを共通化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "@app/app-routing.module";
 import { AppComponent } from "@app/app.component";
+import { DashboardComponent } from "@app/components/dashboard/dashboard.component";
+import { ErrorComponent } from "@app/components/error/error.component";
 
 import { SharedModule } from "@shared/shared.module";
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, DashboardComponent, ErrorComponent],
     imports: [SharedModule, AppRoutingModule, BrowserModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: "app-card",
+    templateUrl: "./card.component.html",
+    styleUrls: ["./card.component.scss"],
+})
+export class CardComponent {
+    /**
+     * タイトル
+     */
+    @Input() title: string | null = null;
+
+    constructor() {}
+}
