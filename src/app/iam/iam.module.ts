834c81831c7542ae96bf8b0aa465888fe011f0ea
fkd6280 <theearth6280@outlook.jp>
2022-10-02 23:37:23 +0900

    [fix] usernamePipeをapp/shared/pipesに移動

--- src/app/iam/iam.module.ts
+++ src/app/iam/iam.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from "@angular/core";
 
 import { UserGroupsComponent } from "@iam/components/user-groups/user-groups.component";
 import { UsersComponent } from "@iam/components/users/users.component";
-import { UsernamePipe } from "@iam/shared/pipes/username.pipe";
 
 import { SharedModule } from "@shared/shared.module";
 
 @NgModule({
-    declarations: [UserGroupsComponent, UsersComponent, UsernamePipe],
+    declarations: [UserGroupsComponent, UsersComponent],
     imports: [SharedModule],
 })
 export class IamModule {}
