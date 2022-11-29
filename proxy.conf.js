3ce462c3a166aaf16e2948af33220846fb962094
averak <abe12@mccc.jp>
2022-11-30 02:40:09 +0900

    #104 リバースプロキシ先をkubernetes設定に追従

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,20 +1,7 @@
 module.exports = [
     {
-        context: ["/iam/api"],
-        target: "http://localhost:8081",
-        pathRewrite: { "^/iam/api": "/api" },
-        secure: false,
-    },
-    {
-        context: ["/paymaster/api"],
-        target: "http://localhost:8082",
-        pathRewrite: { "^/paymaster/api": "/api" },
-        secure: false,
-    },
-    {
-        context: ["/reminder/api"],
-        target: "http://localhost:8083",
-        pathRewrite: { "^/reminder/api": "/api" },
+        context: ["/iam/api", "/paymaster/api", "/reminder/api"],
+        target: "http://localhost:30000",
         secure: false,
     },
 ];
--- src/environments/environment.dev.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    IS_AUTHENTICATED_KEY: "Authenticated",
-
-    // 各種APIのルートURLを指定
-    IAM_API_ROOT_URL: "https://api.abelab.dev/membership-console/iam",
-    PURCHASE_REQUEST_API_ROOT_URL: "https://api.abelab.dev/membership-console/purchase-request",
-};
